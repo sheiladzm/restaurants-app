8dc9ad6e295db48ca36d637220199fe58f523c2a
sheiladzm <sheilamaediezmo@gmail.com>
2022-05-09 12:19:25 -0700

    Initiate variables and provide getters and setters

--- restaurants_app_java/src/main/java/com/sheila/restaurants_app/model/Address.java
+++ restaurants_app_java/src/main/java/com/sheila/restaurants_app/model/Address.java
@@ -1,4 +1,53 @@
 package com.sheila.restaurants_app.model;
 
 public class Address {
+
+    private int addressId;
+    private String name;
+    private String foodTypes;
+    private String schedule;
+    private int restaurantId;
+
+    //constructor
+    public Address() {
+    }
+
+    //getters and setters to access private variables
+
+    public int getAddressId() {
+        return addressId;
+    }
+
+    public void setAddressId(int addressId) {
+        this.addressId = addressId;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFoodTypes() {
+        return foodTypes;
+    }
+    public void setFoodTypes(String foodTypes) {
+        this.foodTypes = foodTypes;
+    }
+
+    public String getSchedule() {
+        return schedule;
+    }
+    public void setSchedule(String schedule) {
+        this.schedule = schedule;
+    }
+
+    public int getRestaurantId() {
+        return restaurantId;
+    }
+    public void setRestaurantId(int restaurantId) {
+        this.restaurantId = restaurantId;
+    }
+
 }
--- restaurants_app_java/src/main/java/com/sheila/restaurants_app/model/Restaurant.java
+++ restaurants_app_java/src/main/java/com/sheila/restaurants_app/model/Restaurant.java
@@ -1,4 +1,28 @@
 package com.sheila.restaurants_app.model;
 
 public class Restaurant {
+
+    private int restaurantId;
+    private String name;
+
+    //constructor
+    public Restaurant() {
+    }
+
+    //getters and setters to access private variables
+
+    public int getRestaurantId() {
+        return restaurantId;
+    }
+    public void setRestaurantId(int restaurantId) {
+        this.restaurantId = restaurantId;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
 }
