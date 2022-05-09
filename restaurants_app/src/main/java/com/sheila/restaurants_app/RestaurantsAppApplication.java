a82a7f1f9da8d7d7ff5925ae69f86eb494269d77
sheiladzm <sheilamaediezmo@gmail.com>
2022-05-09 01:24:57 -0700

    Add some sql queries

---
+++ restaurants_app/src/main/java/com/sheila/restaurants_app/RestaurantsAppApplication.java
@@ -0,0 +1,13 @@
+package com.sheila.restaurants_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantsAppApplication.class, args);
+    }
+
+}
