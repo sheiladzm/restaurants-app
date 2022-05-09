3418b746f978d69a00326518db79058b78973577
sheiladzm <sheilamaediezmo@gmail.com>
2022-05-09 12:41:17 -0700

    Create abstract methods in dao interfaces

--- restaurants_app_java/src/main/java/com/sheila/restaurants_app/dao/AddressDao.java
+++ restaurants_app_java/src/main/java/com/sheila/restaurants_app/dao/AddressDao.java
@@ -1,4 +1,22 @@
 package com.sheila.restaurants_app.dao;
 
+import com.sheila.restaurants_app.model.Address;
+
+import java.util.List;
+
+//Interface for complete abstraction, hiding details such as sql statements to be made in the JdbcDao classes
 public interface AddressDao {
+
+    //Abstract methods to be used in the AddressesController
+
+    List<Address> getAddresses();
+
+    Address getAddress(int addressId);
+
+    void addAddress(Address addressToAdd);
+
+    void deleteAddress(int addressId);
+
+    void updateAddress(int addressId, Address addressToUpdate);
+
 }
--- restaurants_app_java/src/main/java/com/sheila/restaurants_app/dao/RestaurantDao.java
+++ restaurants_app_java/src/main/java/com/sheila/restaurants_app/dao/RestaurantDao.java
@@ -1,4 +1,23 @@
 package com.sheila.restaurants_app.dao;
 
+
+import com.sheila.restaurants_app.model.Restaurant;
+
+import java.util.List;
+
+//Interface for complete abstraction, hiding details such as sql statements to be made in the JdbcDao classes
 public interface RestaurantDao {
+
+    //Abstract methods to be used in the RestaurantsController
+
+    List<Restaurant> getRestaurants();
+
+    Restaurant getRestaurant(int restaurantId);
+
+    void addRestaurant(Restaurant restaurantToAdd);
+
+    void deleteRestaurant(int restaurantId);
+
+    void updateRestaurant(int restaurantId, Restaurant restaurantToUpdate);
+
 }
