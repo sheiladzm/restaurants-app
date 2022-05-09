d09695d489dac0ae94bbd7dd450fbc2c06089940
sheiladzm <sheilamaediezmo@gmail.com>
2022-05-09 11:57:53 -0700

    Create custom exceptions for Not Found status

--- restaurants_app_java/src/main/java/com/sheila/restaurants_app/exceptions/AddressNotFoundException.java
+++ restaurants_app_java/src/main/java/com/sheila/restaurants_app/exceptions/AddressNotFoundException.java
@@ -1,4 +1,18 @@
 package com.sheila.restaurants_app.exceptions;
 
-public class AddressNotFoundException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class AddressNotFoundException extends RuntimeException {
+
+    private static final long serialVersionUID = -1106120769522679372L;
+
+    public AddressNotFoundException() {
+    }
+
+    public AddressNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
 }
--- restaurants_app_java/src/main/java/com/sheila/restaurants_app/exceptions/RestaurantNotFoundException.java
+++ restaurants_app_java/src/main/java/com/sheila/restaurants_app/exceptions/RestaurantNotFoundException.java
@@ -1,4 +1,18 @@
 package com.sheila.restaurants_app.exceptions;
 
-public class RestaurantNotFoundException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class RestaurantNotFoundException extends RuntimeException {
+
+    private static final long serialVersionUID = -1106120769522679372L;
+
+    public RestaurantNotFoundException() {
+    }
+
+    public RestaurantNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
 }
