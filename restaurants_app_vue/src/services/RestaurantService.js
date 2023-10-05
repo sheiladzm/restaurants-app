9ba03bea57c8c5cebb75ba6c14ce62c92667052e
sheiladzm <101483745+sheiladzm@users.noreply.github.com>
2023-10-05 16:45:04 -0700

    Clean up RestaurantService.js
    Remove spaces and commented out code

--- restaurants_app_vue/src/services/RestaurantService.js
+++ restaurants_app_vue/src/services/RestaurantService.js
@@ -1,26 +1,18 @@
 import axios from 'axios';
 
-// const http = axios.create({
-//   baseURL: 'http://localhost:8080/api',
-// });
-
 export default {
-
   //View all restaurants
   list() {
     return axios.get('http://localhost:8080/api/allRestaurants/');
   },
-
   //View a specific restaurant
   get(restaurantId) {
     return axios.get(`http://localhost:8080/api/restaurant/${restaurantId}`);
   },
-  
   //Add new restaurant
   create(restaurant) {
     return axios.post('http://localhost:8080/api/addRestaurant', restaurant);
   },
-
   //Update restaurant name
   update(restaurant) {
     return axios.put(
@@ -28,5 +20,4 @@ export default {
       restaurant
     );
   },
-
 };
