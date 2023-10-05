e6e6bd6f77d7a0c1a09dfd5fad0c9102e6ffb5e6
sheiladzm <101483745+sheiladzm@users.noreply.github.com>
2023-10-05 15:27:48 -0700

    Clean up AddressDao.java
    Remove spaces and commented out code

--- restaurants_app_java/src/main/java/com/sheila/restaurants_app/dao/AddressDao.java
+++ restaurants_app_java/src/main/java/com/sheila/restaurants_app/dao/AddressDao.java
@@ -1,28 +1,20 @@
 package com.sheila.restaurants_app.dao;
 
 import com.sheila.restaurants_app.model.Address;
-
 import java.util.List;
 
 //Interface for complete abstraction, hiding details such as sql statements to be made in the JdbcDao classes
 public interface AddressDao {
 
-    //Abstract methods to be used in the AddressesController
-
     List<Address> getAddresses();
 
     List<Address> getAddressesByRestaurant(int restaurantId);
 
-//    List<Address> getAddressesWithRestaurants();
-
     Address getAddress(int addressId);
-
-//    Address getAddressMoreDetails(int addressId);
 
     void addAddress(Address addressToAdd);
 
     void deleteAddress(int addressId);
 
     void updateAddress(int addressId, Address addressToUpdate);
-
 }
