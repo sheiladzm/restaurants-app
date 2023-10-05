e7730c8020241cb93cd2b55d120e5326d0d8e516
sheiladzm <101483745+sheiladzm@users.noreply.github.com>
2023-10-05 15:30:16 -0700

    Clean up AddressJdbcDao.java
    Remove spaces and commented out code

--- restaurants_app_java/src/main/java/com/sheila/restaurants_app/dao/AddressJdbcDao.java
+++ restaurants_app_java/src/main/java/com/sheila/restaurants_app/dao/AddressJdbcDao.java
@@ -5,13 +5,11 @@ import com.sheila.restaurants_app.model.Address;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Component;
-
 import javax.sql.DataSource;
 import java.util.ArrayList;
 import java.util.List;
 
 //Houses connection and concrete methods to touch the database - addresses table
-
 @Component
 public class AddressJdbcDao implements AddressDao {
 
@@ -25,77 +23,42 @@ public class AddressJdbcDao implements AddressDao {
 
     //Transform database row into an object
     private Address mapRowToAddress(SqlRowSet result) {
-
         Address retrievedAddress = new Address();
-
         retrievedAddress.setAddressId(result.getInt("address_id"));
         retrievedAddress.setName(result.getString("name"));
         retrievedAddress.setFoodTypes(result.getString("food_types"));
         retrievedAddress.setSchedule(result.getString("schedule"));
         retrievedAddress.setRestaurantId(result.getInt("restaurant_id"));
-
         return retrievedAddress;
-
     }
 
     //Get all addresses from the database and build a list
     @Override
     public List<Address> getAddresses() {
-
         List<Address> addressList = new ArrayList<>();
-
         String sql = "select * from addresses";
         SqlRowSet results = template.queryForRowSet(sql);
         while (results.next()) {
             addressList.add(mapRowToAddress(results));
         }
-
         return addressList;
-
     }
 
     @Override
     public List<Address> getAddressesByRestaurant(int restaurantId) {
-
         List<Address> addressList = new ArrayList<>();
-
         String sql = "select * from addresses where restaurant_id = ?";
         SqlRowSet results = template.queryForRowSet(sql, restaurantId);
         while (results.next()) {
             addressList.add(mapRowToAddress(results));
         }
-
         return addressList;
-
     }
-
-
-
-//    @Override
-//    public List<Address> getAddressesWithRestaurants() {
-//
-//        List<Address> addressListWithRestaurants = new ArrayList<>();
-//
-//        String sql = "select restaurants.name, addresses.name\n" +
-//                "from addresses\n" +
-//                "left join restaurants on restaurants.restaurant_id = addresses.restaurant_id\n" +
-//                "group by restaurants.name, addresses.name\n" +
-//                "order by restaurants.name;";
-//        SqlRowSet results = template.queryForRowSet(sql);
-//        while (results.next()) {
-//            addressListWithRestaurants.add(mapRowToAddress(results));
-//        }
-//
-//        return addressListWithRestaurants;
-//
-//    }
 
     //Get a specific address from the database
     @Override
     public Address getAddress(int addressId) {
-
         Address address;
-
         String sql = "select * from addresses where address_id = ?";
         SqlRowSet result = template.queryForRowSet(sql, addressId);
         if (result.next()) {
@@ -104,58 +67,27 @@ public class AddressJdbcDao implements AddressDao {
         else {
             throw new AddressNotFoundException();
         }
-
         return address;
-
     }
-
-//    //Get a specific address from the database, displaying the restaurant name, addresses, food types, and schedule
-//    @Override
-//    public Address getAddressMoreDetails(int addressId) {
-//
-//        Address addressMoreDetails;
-//
-//        String sql = "select restaurants.name, addresses.name, addresses.food_types, addresses.schedule\n" +
-//                "from addresses\n" +
-//                "full join restaurants on restaurants.restaurant_id = addresses.restaurant_id\n" +
-//                "where addresses.address_id = ?;";
-//        SqlRowSet result = template.queryForRowSet(sql, addressId);
-//        if (result.next()) {
-//            addressMoreDetails = mapRowToAddress(result);
-//        }
-//        else {
-//            throw new AddressNotFoundException();
-//        }
-//
-//        return addressMoreDetails;
-//
-//    }
 
     //Add a new address to the database
     @Override
     public void addAddress(Address addressToAdd) {
-
         String sql = "insert into addresses (name, food_types, schedule, restaurant_id) values (?, ?, ?, ?)";
         template.update(sql, addressToAdd.getName(), addressToAdd.getFoodTypes(), addressToAdd.getSchedule(), addressToAdd.getRestaurantId());
-
     }
 
     //Delete a specific address from the database
     @Override
     public void deleteAddress(int addressId) {
-
         String sql = "delete from addresses where address_id = ?";
         template.update(sql, addressId);
-
     }
 
     //Update a specific address from the database
     @Override
     public void updateAddress(int addressId, Address addressToUpdate) {
-
         String sql = "update addresses set name = ?, food_types = ?, schedule = ?, restaurant_id = ?";
         template.update(sql, addressToUpdate.getName(), addressToUpdate.getFoodTypes(), addressToUpdate.getSchedule(), addressToUpdate.getRestaurantId());
-
     }
-
 }
